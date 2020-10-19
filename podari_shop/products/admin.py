8199780f163a197c68edabb009794997578d8b11
Arthur <artur.aleshchyk@gmail.com>
2020-10-19 12:38:09 +0300

    register Product and Category models in admin.py

--- podari_shop/products/admin.py
+++ podari_shop/products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Product
+from products.models import Category
+
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
