2a705b0468ffff6f18d8babd38201eb3234e014c
Arthur <artur.aleshchyk@gmail.com>
2020-10-19 18:09:22 +0300

    add cards for product/content block

--- podari_shop/products/views.py
+++ podari_shop/products/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from products.models import Product, Category
+
 
 def index(req):
-    return render(req, 'products/index.html')
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    return render(req, 'products/index.html', {
+        'categories': categories,
+        'products': products,
+        'title': 'Главная'
+    })
