beaac322f6dfe2eb51ef623722b9ea4f26c50ffe
Arthur <artur.aleshchyk@gmail.com>
2020-10-21 00:58:58 +0300

    add last added products

--- podari_shop/products/views.py
+++ podari_shop/products/views.py
@@ -7,8 +7,11 @@ from products.models import Product, Category
 def index(req):
     categories = Category.objects.all()
     products = Product.objects.all()
+    last_products = Product.objects.order_by('pk').reverse()[:8]
+
     return render(req, 'products/index.html', {
         'categories': categories,
         'products': products,
-        'title': 'Главная'
+        'title': 'Главная',
+        'last_products': last_products
     })
